4fa07b8bf45e0e3ab308e0951ef1efd2963eace0
nasicseiiit <nasicseiiit@gmail.com>
2020-06-19 22:09:34 +0530

    Added test case for get_cli_arguments method.

---
+++ test/test_cli_arguments.py
@@ -0,0 +1,23 @@
+
+import unittest
+
+from app.input_processor.cli_rguments import get_cli_arguments
+from app.input_processor.input_data import get_file_data
+
+
+class test_for_cli_arguments(unittest.TestCase):
+    '''
+    test case to get the file name
+    '''
+    def test_get_file_name(self):
+        try:
+            expected_file_name = "../../input_data/input_file"
+            actual_file_name = get_cli_arguments()
+            print(actual_file_name)
+            self.assertEqual(expected_file_name,actual_file_name)
+        except:
+            IOError
+
+
+if __name__ == '__main__':
+    unittest.main()
